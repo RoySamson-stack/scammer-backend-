247df3e38961975c8ae15fe53b2ae677cd57889a
RoySamson-stack <rsam9009@gmail.com>
2025-08-08 09:04:52 +0300

    update

--- src/index.js
+++ src/index.js
@@ -4,48 +4,26 @@ const config = require('./config/config');
 const logger = require('./config/logger');
 
 let server;
-
-mongoose.connect(config.mongoose.url, config.mongoose.options)
-  .then(() => {
-    logger.info('Connected to MongoDB');
-    
-    const port = config.port || process.env.PORT || 3000;
-    server = app.listen(port, '0.0.0.0', () => {
-      logger.info(`Listening to port ${port}`);
-    });
-    
-    server.on('error', (error) => {
-      logger.error('Server error:', error);
-      if (error.code === 'EADDRINUSE') {
-        logger.error(`Port ${port} is already in use`);
-      }
-    });
-  })
-  .catch((error) => {
-    logger.error('MongoDB connection error:', error);
-    process.exit(1);
+mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
+  logger.info('Connected to MongoDB');
+  server = app.listen(config.port, () => {
+    logger.info(`Listening to port ${config.port}`);
   });
+});
 
 const exitHandler = () => {
   if (server) {
     server.close(() => {
       logger.info('Server closed');
-      // Close mongoose connection
-      mongoose.connection.close(false, () => {
-        logger.info('MongoDB connection closed');
-        process.exit(0); // Exit with success code instead of 1
-      });
+      process.exit(1);
     });
   } else {
-    mongoose.connection.close(false, () => {
-      logger.info('MongoDB connection closed');
-      process.exit(0);
-    });
+    process.exit(1);
   }
 };
 
 const unexpectedErrorHandler = (error) => {
-  logger.error('Unexpected error:', error);
+  logger.error(error);
   exitHandler();
 };
 
@@ -54,10 +32,7 @@ process.on('unhandledRejection', unexpectedErrorHandler);
 
 process.on('SIGTERM', () => {
   logger.info('SIGTERM received');
-  exitHandler();
-});
-
-process.on('SIGINT', () => {
-  logger.info('SIGINT received');
-  exitHandler();
+  if (server) {
+    server.close();
+  }
 });
